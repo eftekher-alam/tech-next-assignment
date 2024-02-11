5dd7ac72591f550711daa04d90aa74e72b66422d
eftekher-alam <eftekher@yahoo.com>
2024-02-12 01:04:12 +0600

    search summary is added

--- src/pages/users/Users.jsx
+++ src/pages/users/Users.jsx
@@ -9,6 +9,7 @@ const Users = () => {
     const [isLoading, setIsLoading] = useState(true);
     const [fetchedUsers, setFetchedUsers] = useState([]);
     const [users, setUsers] = useState([]);
+    const [searchSummery, setSearchSummery] = useState("");
 
     useEffect(() => {
         axios.get("https://dummyjson.com/users")
@@ -22,16 +23,16 @@ const Users = () => {
     const handlerSort = (e) => {
         const sortParameter = e.target.value;
         if (sortParameter === "Name") {
-            const usersSortedbyName = [...users].sort((a, b) => a?.firstName.toLowerCase().localeCompare(b?.firstName.toLowerCase()))
-            setUsers(usersSortedbyName)
+            const usersSortedByName = [...users].sort((a, b) => a?.firstName.toLowerCase().localeCompare(b?.firstName.toLowerCase()))
+            setUsers(usersSortedByName)
         }
         else if (sortParameter === "Email") {
-            const usersSortedbyName = [...users].sort((a, b) => a?.email.toLowerCase().localeCompare(b?.email.toLowerCase()))
-            setUsers(usersSortedbyName)
+            const usersSortedByEmail = [...users].sort((a, b) => a?.email.toLowerCase().localeCompare(b?.email.toLowerCase()))
+            setUsers(usersSortedByEmail)
         }
         else if (sortParameter === "Company") {
-            const usersSortedbyName = [...users].sort((a, b) => a?.company?.name.toLowerCase().localeCompare(b.company?.name.toLowerCase()))
-            setUsers(usersSortedbyName)
+            const usersSortedByCompany = [...users].sort((a, b) => a?.company?.name.toLowerCase().localeCompare(b.company?.name.toLowerCase()))
+            setUsers(usersSortedByCompany)
         }
 
     }
@@ -40,6 +41,10 @@ const Users = () => {
         const searchText = document.getElementById("search").value;
         const filteredUsers = fetchedUsers.filter((user) => (user.firstName + user.lastName).toLowerCase().includes(searchText.toLowerCase()));
         setUsers(filteredUsers);
+        if (searchText.length)
+            setSearchSummery(`Results for ${searchText}, ${filteredUsers?.length} items found.`);
+        else
+            setSearchSummery("");
         document.getElementById("search").value = "";
     }
 
@@ -50,7 +55,7 @@ const Users = () => {
                 isLoading && <LoadingAnimation></LoadingAnimation>
             }
             {
-                !isLoading && <div className="flex justify-center items-center gap-5 md:gap-10 pb-10 flex-wrap" data-aos="zoom-out">
+                !isLoading && <div className="flex justify-center items-center gap-5 md:gap-10 pb-7 flex-wrap" data-aos="zoom-out">
                     <div className="custom-input rounded-none py-1 w-64" >
                         <div className="flex">
                             <input id="search" type='text' autoComplete="off" placeholder="Search by user's name" className="outline-none pl-3 text-black" />
@@ -65,11 +70,17 @@ const Users = () => {
                     <div className="custom-input grid grid-cols-2 px-3 py-1 gap-1 w-64">
                         <label className='font-medium text-[#ff6c6c]'><span className="flex items-center gap-1"><FaSortAlphaDown /> Sort by</span></label>
                         <select className="outline-none w-full font-medium text-[#7d5fff]" onChange={handlerSort}>
+                            <option disabled selected>Pick Option</option>
                             <option>Name</option>
                             <option>Email</option>
                             <option>Company</option>
                         </select>
                     </div>
+
+                </div>
+            }
+            {
+                searchSummery && <div className="w-full text-center font-semibold text-gray-400 pb-7">{searchSummery}
                 </div>
             }
             <div className="grid grid-cols-1 md:grid-cols-2  lg:grid-cols-3 xl:grid-cols-4  gap-10">
