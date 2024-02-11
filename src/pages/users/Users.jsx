cd1a199c087788b7ca15c8056cc3a238d0ad5d01
eftekher-alam <eftekher@yahoo.com>
2024-02-11 21:08:40 +0600

    Loading skeleton is added on users page

--- src/pages/users/Users.jsx
+++ src/pages/users/Users.jsx
@@ -1,20 +1,27 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import UserCard from './../../components/user_card/UserCard';
+import LoadingAnimation from "../../components/loading_animation/LoadingAnimation";
 
 
 const Users = () => {
-
+    const [isLoading, setIsLoading] = useState(true);
     const [users, setUsers] = useState([]);
 
     useEffect(() => {
         axios.get("https://dummyjson.com/users")
-            .then(res => setUsers(res.data.users))
+            .then(res => {
+                setUsers(res.data.users);
+                setIsLoading(false);
+            })
     }, [])
 
 
     return (
         <div className="m-10">
+            {
+                isLoading && <LoadingAnimation></LoadingAnimation>
+            }
             <div className="grid grid-cols-1 md:grid-cols-2  lg:grid-cols-3 xl:grid-cols-4  gap-10">
                 {
                     users.map((user, index) => <UserCard key={index} user={user}></UserCard>)
