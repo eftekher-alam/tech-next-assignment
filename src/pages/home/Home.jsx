f3333fd8cf080a02b7b45675e02dd8d143bff267
eftekher-alam <eftekher@yahoo.com>
2024-02-11 14:07:06 +0600

    Folder structure has been changed

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
-import NavBar from "../../shared_pages/nav_bar/NavBar";
-import Footer from "../../shared_pages/footer/Footer";
+import NavBar from './../shared_pages/nav_bar/NavBar';
+import Footer from './../shared_pages/footer/Footer';
+
 
 const Home = () => {
     return (
---
+++ src/pages/users/Users.jsx
@@ -0,0 +1,11 @@
+
+
+const Users = () => {
+    return (
+        <div>
+            Here will show all the users list.
+        </div>
+    );
+};
+
+export default Users;
